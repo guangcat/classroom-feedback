cd874c362563dbb083cbf9270cde0c89bcc3595b
GuangGuangCat <1984614424@qq.com>
2026-04-04 13:32:22 +0800

    第1天：Spring Boot骨架 + Hello接口 + 数据库表结构

---
+++ src/main/java/com/example/classroomfeedback/ClassroomFeedbackApplication.java
@@ -0,0 +1,12 @@
+package com.example.classroomfeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomFeedbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassroomFeedbackApplication.class, args);
+    }
+}
